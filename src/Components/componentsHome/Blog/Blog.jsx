372f97407e095392c9a62ca1743fcd966d28db2e
devcleber1 <clebinhoalves2011@gmail.com>
2025-09-15 12:51:16 -0300

    Remoção do button do componente blog, para atualizar depois

--- src/Components/componentsHome/Blog/Blog.jsx
+++ src/Components/componentsHome/Blog/Blog.jsx
@@ -17,44 +17,6 @@ export default function BlogSection() {
         </div>
 
         <div className="text-center mt-12">
-          <motion.div whileHover={{ scale: 1.05 }}>
-            <Link
-              to="/blog"
-              className="
-                inline-flex
-                items-center
-                justify-center
-                max-w-xs
-                w-full
-                px-5
-                py-5
-                text-[16px]
-                font-semibold
-                text-white
-                rounded-lg
-                bg-gradient-to-r
-                from-yellow-400
-                to-yellow-500
-                shadow-lg
-                hover:from-yellow-500
-                hover:to-yellow-600
-                hover:scale-105
-                active:scale-95
-                transition
-                duration-300
-                ease-in-out
-                cursor-pointer
-                select-none
-                mx-auto
-                gap-2
-              "
-            >
-              Saiba mais
-              <motion.span className="inline-flex items-center transition-transform duration-300 group-hover:translate-x-1">
-                <ArrowRight size={18} />
-              </motion.span>
-            </Link>
-          </motion.div>
         </div>
       </div>
     </section>
