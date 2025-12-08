084902173feff41e73c68aafd0a3e1a2553b817b
devcleber1 <clebinhoalves2011@gmail.com>
2025-12-08 20:28:20 -0300

    Bug

--- src/Components/componentsHome/Partners/Partners.jsx
+++ src/Components/componentsHome/Partners/Partners.jsx
@@ -8,6 +8,7 @@ import Uni from '../../../assets/unissuam.png';
 import Cristo from '../../../assets/cristo.png';
 import Bestacai from '../../../assets/bestacai.png';
 import Vero from '../../../assets/vero.png';
+import Rs from '../../../assets/rs.png';
 
 const partners = [
   { src: Flamengo, alt: 'Flamengo' },
@@ -17,6 +18,7 @@ const partners = [
   { src: Cristo, alt: 'Cristo Redentor' },
   { src: Bestacai, alt: 'Bestacai' },
   { src: Vero, alt: 'Vero' },
+  { src: Rs, alt: 'RS' },
 ];
 
 // Animação elegante com escala, opacidade e subida
